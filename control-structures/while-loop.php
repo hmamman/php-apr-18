0138b03719e371ec99182a16ca882b7b459b83dc
hussaini9 <hussaini@cloud9.ng>
2018-04-25 16:10:14 +0100

    f

---
+++ control-structures/while-loop.php
@@ -0,0 +1,12 @@
+<?php 
+//ASSIGNMEN
+/*
+1) Refactor below code to use while loop
+2) Print all prime number from 1 to 100
+*/
+for($i = 1; $i <= 10; $i++) {
+    $mode = $i % 2;
+    if ($mode != 0) {
+        echo "{$i} <br>";
+    }
+}
